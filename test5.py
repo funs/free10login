6226fef696915b6336e8f7a0b9e45ca4b2ec5208
funs <pc904b28@yahoo.com.tw>
2015-03-25 02:28:06 +0800

    uri of test5 revised.

--- test5.py
+++ test5.py
@@ -7,7 +7,8 @@ from urlparse import urlparse, parse_qs
 #uri = 'http://free10.com.tw/10/hotspotlogin.php?res=success&uamip=192.168.182.1&uamport=3990&uid=0911220979&userurl=http://www.google.com.tw&nasid=B0-C7-45-6E-27-37&mac=00-D0-41-C3-31-E6#page_welcome'
 #uri = 'https://www.facebook.com/holinetw'
 #uri = 'http://www.google.com.tw'
-uri = 'http://free10.com.tw/10/hotspotlogin.php?res=notyet&uamip=192.168.182.1&uamport=3990&challenge=997b2c88b8a105922bd8c0e190e45e08&userurl=http%3a%2f%2fwww.google.com.tw%2f&nasid=B0-C7-45-6E-27-37&mac=00-D0-41-C3-31-E6'
+#uri = 'http://free10.com.tw/10/hotspotlogin.php?res=notyet&uamip=192.168.182.1&uamport=3990&challenge=997b2c88b8a105922bd8c0e190e45e08&userurl=http%3a%2f%2fwww.google.com.tw%2f&nasid=B0-C7-45-6E-27-37&mac=00-D0-41-C3-31-E6'
+uri = 'http://free10.com.tw/10/hotspotlogin.php?res=already&uamip=192.168.182.1&uamport=3990&userurl=&nasid=B0-C7-45-6E-27-37&mac=00-D0-41-C3-31-E6#page_welcome'
 request = urllib2.Request(uri)
 request.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
 request.add_header('X-Requested-With','XMLHttpRequest')
@@ -20,4 +21,5 @@ redirected = response.geturl()==uri
 print(redirected)
 html = response.read()
 print(response.geturl())
+print(html)
 
